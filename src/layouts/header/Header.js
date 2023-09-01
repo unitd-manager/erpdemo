fae1332053d413193c4a93dff287ea684130d342
r-jadhav <renukajadhav2211@gmail.com>
2023-09-01 12:57:57 +0530

    Account Map

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -80,8 +80,8 @@ const Header = () => {
         </NavItem> */}
        
         
-          <Label >
-            TMS
+       <Label className='text-white'>
+            ERP Demo
           </Label>
       {/* </Nav> */}
       {/******************************/}
