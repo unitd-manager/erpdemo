0090aeb098cf4a3162f28d9d52a47b40f297baad
gokilaunitd <gokila@unitdtechnologies.com>
2023-09-07 15:40:44 +0530

    Tas

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,12 +1,17 @@
-import axios from 'axios'
+import axios from 'axios';
 
+// Define the base URL(s) conditionally
+let baseURL;
+
+if (process.env.NODE_ENV === 'production') {
+  baseURL = 'http://43.228.126.245:5001';
+} else {
+  baseURL = 'http://localhost:5001';
+}
+
+console.log('NODE_ENV:', process.env.NODE_ENV);
 const api = axios.create({
- baseURL: 'http://43.228.126.245:5001',
-// baseURL: 'http://localhost:5001',
-
+  baseURL, // Use object shorthand notation here
 });
 
-
-
-
-export default api
+export default api;
