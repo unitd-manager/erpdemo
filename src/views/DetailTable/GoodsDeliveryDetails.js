ce8a3b02213b1c8203b9ba572260d0ac43f34a6b
gokilaunitd <gokila@unitdtechnologies.com>
2023-09-11 11:01:47 +0530

    goods

--- src/views/DetailTable/GoodsDeliveryDetails.js
+++ src/views/DetailTable/GoodsDeliveryDetails.js
@@ -7,25 +7,13 @@ import ComponentCard from '../../components/ComponentCard';
 import api from '../../constants/api';
 import message from '../../components/Message';
 import creationdatetime from '../../constants/creationdatetime';
-import TenderCompanyDetails from '../../components/TenderTable/TenderCompanyDetails';
 import AppContext from '../../context/AppContext';
 
 const GoodsDeliveryDetails = () => {
-  const [company, setCompany] = useState();
   const [enquirycode, setEnquiryCode] = useState();
-  const [allCountries, setallCountries] = useState();
-  const [modal, setModal] = useState(false);
   const { id } = useParams();
   const navigate = useNavigate();
-  const toggle = () => {
-    setModal(!modal);
-  };
-  //Api call for getting company dropdown
-  const getCompany = () => {
-    api.get('/company/getCompany').then((res) => {
-      setCompany(res.data.data);
-    });
-  };
+
 
   //Api call for getting Enquiry dropdown
   const getOrderCode = () => {
@@ -34,70 +22,15 @@ const GoodsDeliveryDetails = () => {
     });
   };
 
-  //Logic for adding company in db
-  const [companyInsertData, setCompanyInsertData] = useState({
-    company_name: '',
-    address_street: '',
-    address_town: '',
-    address_country: '',
-    address_po_code: '',
-    phone: '',
-    fax: '',
-    website: '',
-    supplier_type: '',
-    industry: '',
-    company_size: '',
-    source: '',
-  });
-
-  const handleInputs = (e) => {
-    setCompanyInsertData({ ...companyInsertData, [e.target.name]: e.target.value });
-  };
-
-  const insertCompany = () => {
-    if (
-      companyInsertData.company_name !== '' &&
-      companyInsertData.address_street !== '' &&
-      companyInsertData.address_po_code !== '' &&
-      companyInsertData.address_country !== ''
-    ) {
-      api
-        .post('/company/insertCompany', companyInsertData)
-        .then(() => {
-          message('Company inserted successfully.', 'success');
-          getCompany();
-          window.location.reload();
-        })
-        .catch(() => {
-          message('Network connection error.', 'error');
-        });
-    } else {
-      message('Please fill all required fields.', 'warning');
-    }
-  };
-
   //Logic for adding tender in db
   const [tenderForms, setTenderForms] = useState({
     order_code: '',
-    company_id: '',
-    company_name: '',
   });
 
   const handleInputsTenderForms = (e) => {
     setTenderForms({ ...tenderForms, [e.target.name]: e.target.value });
   };
 
-  //Api for getting all countries
-  const getAllCountries = () => {
-    api
-      .get('/clients/getCountry')
-      .then((res) => {
-        setallCountries(res.data.data);
-      })
-      .catch(() => {
-        message('Country Data Not Found', 'info');
-      });
-  };
   //const[tenderDetails,setTenderDetails]=useState();
   const getTendersById = () => {
     api
@@ -112,9 +45,9 @@ const GoodsDeliveryDetails = () => {
   const { loggedInuser } = useContext(AppContext);
 
   //console.log(tenderDetails);
-  const insertgoodsDelivery = () => {
+  const insertgoodsDelivery = (code) => {
     if (tenderForms.company_id !== '' && tenderForms.goods_delivery_date !== '') {
-      //tenderForms.order_code = code;
+      tenderForms.goods_delivery_code = code;
       tenderForms.creation_date = creationdatetime;
       tenderForms.created_by = loggedInuser.first_name;
       api
@@ -135,22 +68,20 @@ const GoodsDeliveryDetails = () => {
     }
   };
 
-  //QUOTE GENERATED CODE
-  // const generateCode = () => {
-  //   api
-  //     .post('/tender/getCodeValue', { type: 'quote' })
-  //     .then((res) => {
-  //       insertQuote(res.data.data);
-  //     })
-  //     .catch(() => {
-  //       insertQuote('');
-  //     });
-  // };
+ // QUOTE GENERATED CODE
+  const generateCode = () => {
+    api
+      .post('/tender/getCodeValue', { type: 'goodsdelivery' })
+      .then((res) => {
+        insertgoodsDelivery(res.data.data);
+      })
+      .catch(() => {
+        insertgoodsDelivery('');
+      });
+  };
 
   useEffect(() => {
-    getCompany();
     getOrderCode();
-    getAllCountries();
   }, [id]);
 
   return (
@@ -199,45 +130,6 @@ const GoodsDeliveryDetails = () => {
                   </Col>
                 </Row>
               </FormGroup>
-              <FormGroup>
-                <Row>
-                  <Col md="9">
-                    <Label>
-                      Company Name <span className="required"> *</span>{' '}
-                    </Label>
-                    <Input
-                      type="select"
-                      name="company_id"
-                      //value={tenderForms && tenderForms.company_id}
-                      onChange={handleInputsTenderForms}
-                    >
-                      <option>Please Select</option>
-                      {company &&
-                        company.map((ele) => {
-                          return (
-                            <option key={ele.company_id} value={ele.company_id}>
-                              {ele.company_name}
-                            </option>
-                          );
-                        })}
-                    </Input>
-                  </Col>
-                  <Col md="3" className="addNew">
-                    <Label>Add New Name</Label>
-                    <Button color="primary" className="shadow-none" onClick={toggle.bind(null)}>
-                      Add New
-                    </Button>
-                  </Col>
-                </Row>
-              </FormGroup>
-              <TenderCompanyDetails
-                allCountries={allCountries}
-                insertCompany={insertCompany}
-                handleInputs={handleInputs}
-                toggle={toggle}
-                modal={modal}
-                setModal={setModal}
-              ></TenderCompanyDetails>
               <Row>
                 <div className="pt-3 mt-3 d-flex align-items-center gap-2">
                   <Button
@@ -245,7 +137,7 @@ const GoodsDeliveryDetails = () => {
                     color="primary"
                     className="btn mr-2 shadow-none"
                     onClick={() => {
-                      insertgoodsDelivery();
+                      generateCode();
                     }}
                   >
                     Save & Continue
