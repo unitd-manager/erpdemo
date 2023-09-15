a779d5480ffaba39003b9ac5940336959e82419d
smsaf91 <smsaf91@gmail.com>
2023-09-15 13:56:23 +0530

    message

--- src/components/PriceList/PriceEditModal.js
+++ src/components/PriceList/PriceEditModal.js
@@ -97,11 +97,18 @@ const PlanEditModal = ({ planData, editPlanEditModal, setPlanEditModal }) => {
                               <FormGroup>
                                 <Label>Unit</Label>
                                 <Input
-                                  type="text"
+                                  type="select"
                                   name="unit"
                                   onChange={handleInputs}
                                   value={PlaniEdit && PlaniEdit.unit}
-                                />
+                                  >
+                                  <option defaultValue="selected">Please Select</option>
+                                  <option value="KGS">KGS</option>
+                                  <option value="PCS">PCS</option>
+                                  <option value="EA">EA</option>
+                                  <option value="NOS">NOS</option>
+                                  <option value="BOX">BOX</option>
+                                </Input>
                               </FormGroup>
                             </Col>
                             </Row>
