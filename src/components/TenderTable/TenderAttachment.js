b302e23b1cbb4ce0a376e62afcd4fb5cbf8c86e8
Sulfiya786 <sulfiya@unitdtechnologies.com>
2023-09-20 09:00:47 +0530

    push

--- src/components/TenderTable/TenderAttachment.js
+++ src/components/TenderTable/TenderAttachment.js
@@ -31,7 +31,7 @@ export default function TenderAttachment() {
         className="shadow-none"
         color="primary"
         onClick={() => {
-          setRoomName('Tender');
+          setRoomName('Enquiry');
           setFileTypes(['JPG', 'JPEG', 'PNG', 'GIF', 'PDF']);
           dataForAttachment();
           setAttachmentModal(true);
@@ -48,14 +48,14 @@ export default function TenderAttachment() {
     setAttachmentModal={setAttachmentModal}
     roomName={RoomName}
     fileTypes={fileTypes}
-    altTagData="TenderRelated Data"
-    desc="TenderRelated Data"
+    altTagData="EnquiryRelated Data"
+    desc="EnquiryRelated Data"
     recordType="RelatedPicture"
     mediaType={attachmentData.modelType}
     update={update}
     setUpdate={setUpdate}
   />
-  <ViewFileComponentV2 moduleId={id} roomName="Tender" recordType="RelatedPicture" update={update}
+  <ViewFileComponentV2 moduleId={id} roomName="Enquiry" recordType="RelatedPicture" update={update}
     setUpdate={setUpdate} /></div>
   )
 }
