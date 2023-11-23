8b29baf9856e41ec7e449bdf007471bd5eca5582
Muthumari-1993 <119999897+Muthumari-1993@users.noreply.github.com>
2023-11-23 12:27:39 +0530

    invoice

--- src/views/EditData/InvoiceEdit.js
+++ src/views/EditData/InvoiceEdit.js
@@ -2,8 +2,9 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useParams,useNavigate } from 'react-router-dom';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import '../form-editor/editor.scss';
+import * as Icon from 'react-feather';
 import { ToastContainer } from 'react-toastify';
-import { Button, Col, FormGroup, Row } from 'reactstrap';
+import { Row, Col, Form, FormGroup, TabContent, TabPane, Button } from 'reactstrap';
 import BreadCrumbs from '../../layouts/breadcrumbs/BreadCrumbs';
 import message from '../../components/Message';
 import api from '../../constants/api';
@@ -11,23 +12,56 @@ import ComponentCard from '../../components/ComponentCard';
 import InvoiceDetailComp from '../../components/BookingTable/InvoiceDetailComp';
 import creationdatetime from '../../constants/creationdatetime';
 import ComponentCardV2 from '../../components/ComponentCardV2';
-import InvoiceItem from '../../components/BookingTable/InvoiceItem';
-import ItemTable from '../../components/BookingTable/ItemTable';
+import AttachmentModalV2 from '../../components/Tender/AttachmentModalV2';
+import ViewFileComponentV2 from '../../components/ProjectModal/ViewFileComponentV2';
+// import InvoiceItem from '../../components/BookingTable/InvoiceItem';
+// import ItemTable from '../../components/BookingTable/ItemTable';
 import OrderItemTable from '../../components/BookingTable/OrderItemTable';
+import PartialINvoiceEdit from '../../components/BookingTable/PartialINvoiceEdit';
 import AppContext from '../../context/AppContext';
+import Tab from '../../components/project/Tab';
 
 const InvoiceEdit = () => {
   const [bookingDetails, setBookingDetails] = useState({});
-  const [editInvoiceItemData, setEditInvoiceItemData] = useState(false);
-  const [itemDetails, setItemDetails] = useState([]);
+  // const [editInvoiceItemData, setEditInvoiceItemData] = useState(false);
+  // const [itemDetails, setItemDetails] = useState([]);
+  const [partialinvoiceeditmodal, setPartialInvoiceEditModal] = useState(false);
   const [orderitemDetails, setOrderItemDetails] = useState([]);
   const { insertedDataId, orderId } = useParams();
+  const [activeTab, setActiveTab] = useState('1');
+  const [attachmentModal, setAttachmentModal] = useState(false);
+  const [RoomName, setRoomName] = useState('');
+  const [fileTypes, setFileTypes] = useState('');
+  const [attachmentData, setDataForAttachment] = useState({
+    modelType: '',
+  });
+  const [update, setUpdate] = useState(false);
+   const { id } = useParams();
   const { loggedInuser } = useContext(AppContext);
   console.log('order ID:', orderId);
   const navigate = useNavigate();
   const handleInputs = (e) => {
     setBookingDetails({ ...bookingDetails, [e.target.name]: e.target.value });
   };
+
+  // Function to toggle tabs
+  const toggle = (tab) => {
+    if (activeTab !== tab) {
+      setActiveTab(tab);
+    }
+  };
+  const tabs = [
+    { id: '1', name: 'Sales Invoice Item' },
+    { id: '2', name: 'Attachment' },
+  ];
+
+   // Attachment
+   const dataForAttachment = () => {
+    setDataForAttachment({
+      modelType: 'attachment',
+    });
+  };
+
   const generateData = () => {
     // Step 1: Delete old order items by quote_id
     api
@@ -108,16 +142,16 @@ const InvoiceEdit = () => {
       });
   };
  
-  const editItemById = () => {
-    api
-      .post('/invoice/getInvoiceByItemId', { invoice_id: insertedDataId })
-      .then((res) => {
-        setItemDetails(res.data.data);
-      })
-      .catch(() => {
-        //message('Booking Data Not Found', 'info');
-      });
-  };
+  // const editItemById = () => {
+  //   api
+  //     .post('/invoice/getInvoiceByItemId', { invoice_id: insertedDataId })
+  //     .then((res) => {
+  //       setItemDetails(res.data.data);
+  //     })
+  //     .catch(() => {
+  //       //message('Booking Data Not Found', 'info');
+  //     });
+  // };
 
   const getOrderItemById = () => {
     api
@@ -169,7 +203,7 @@ const InvoiceEdit = () => {
 
   useEffect(() => {
     editBookingById();
-    editItemById();
+    // editItemById();
     getOrderItemById();
   }, [insertedDataId]);
 
@@ -180,18 +214,7 @@ const InvoiceEdit = () => {
       <ToastContainer/>
       <ComponentCardV2>
       <Row>
-      <Col>
-              <Button
-                color="primary"
-                className="shadow-none"
-                onClick={() => {
-                  generateData();
-                  
-                }}
-              >
-                Generate Data
-              </Button>
-            </Col>
+      
          <Col>
               <Button
                 color="primary"
@@ -239,12 +262,94 @@ const InvoiceEdit = () => {
           handleInputs={handleInputs}
         />
       </ComponentCard>
-      <InvoiceItem
+      <ComponentCard title="More Details">
+        <ToastContainer></ToastContainer>
+        <Tab toggle={toggle} tabs={tabs} />
+        <TabContent className="p-4" activeTab={activeTab}>
+          <TabPane tabId="1">
+          
+          <Row className="mb-4">
+          <Col md="3">
+              <Button
+                color="primary"
+                className="shadow-none"
+                onClick={() => {
+                  generateData();
+                  
+                }}
+              >
+                Create Full Invoice
+              </Button>
+            </Col>
+          <Col md="3">
+                  <PartialINvoiceEdit
+                  partialinvoiceeditmodal={partialinvoiceeditmodal}
+                  setPartialInvoiceEditModal={setPartialInvoiceEditModal}
+                  SalesInvoiceId={insertedDataId}
+                    ></PartialINvoiceEdit>
+                    <Button
+            className="shadow-none"
+            color="primary"
+            onClick={() => {
+              setPartialInvoiceEditModal(true);
+            }
+            }
+          >
+            Create Partial Invoice
+          </Button>
+        
+      </Col>
+          </Row>   
+          <OrderItemTable
+        orderitemDetails={orderitemDetails}
+        
+       />     
+          </TabPane>
+          <TabPane tabId="2">
+          <Form>
+              <FormGroup>
+                  <Row>
+                    <Col xs="12" md="3" className="mb-3">
+                      <Button
+                        className="shadow-none"
+                        color="primary"
+                        onClick={() => {
+                          setRoomName('SalesInvoice');
+                          setFileTypes(['JPG','JPEG', 'PNG', 'GIF', 'PDF']);
+                          dataForAttachment();
+                          setAttachmentModal(true);
+                        }}
+                      >
+                        <Icon.File className="rounded-circle" width="20" />
+                      </Button>
+                    </Col>
+                  </Row>
+                  <AttachmentModalV2
+                    moduleId={id}
+                    attachmentModal={attachmentModal}
+                    setAttachmentModal={setAttachmentModal}
+                    roomName={RoomName}
+                    fileTypes={fileTypes}
+                    altTagData="SalesInvoiceRequestRelated Data"
+                    desc="SalesInvoiceRequestRelated Data"
+                    recordType="RelatedPicture"
+                    mediaType={attachmentData.modelType}
+                    update={update}
+                    setUpdate={setUpdate}
+                  />
+                  <ViewFileComponentV2 moduleId={id} roomName="SalesInvoice" recordType="RelatedPicture" update={update}
+                    setUpdate={setUpdate}/>
+              </FormGroup>
+            </Form>  
+          </TabPane>
+        </TabContent>
+        </ComponentCard>
+      {/* <InvoiceItem
         editInvoiceItemData={editInvoiceItemData}
         setEditInvoiceItemData={setEditInvoiceItemData}
         invoiceInfo={insertedDataId}
-        ></InvoiceItem>
-      <ComponentCard title="Invoice Items">
+        ></InvoiceItem> */}
+      {/* <ComponentCard title="Invoice Items">
       <Col>
           <Button
             className="shadow-none"
@@ -262,8 +367,8 @@ const InvoiceEdit = () => {
         invoiceInfo={insertedDataId}
        />
       </Row>
-      </ComponentCard>
-      <ComponentCard title="Order Items">
+      </ComponentCard> */}
+      {/* <ComponentCard title="Order Items">
      
         <Row className="border-bottom mb-3">
          <OrderItemTable
@@ -271,7 +376,7 @@ const InvoiceEdit = () => {
         
        />
       </Row>
-      </ComponentCard>
+      </ComponentCard> */}
     </>
   );
 };
--- src/views/smartconTables/SalesInvoice.js
+++ src/views/smartconTables/SalesInvoice.js
@@ -124,7 +124,7 @@ const Invoice = () => {
                   <tr key={element.invoice_id}>
                     <td>{index + 1}</td>
                     <td>
-                      <Link to={`/InvoiceEdit/${element.invoice_id}/${element.order_id}`}>
+                      <Link to={`/InvoiceEdit/${element.invoice_id}/${element.order_id}?tab=1`}>
                         <Icon.Edit2 />
                       </Link>
                     </td>
