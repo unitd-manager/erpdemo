53a7ac15c98b2e223520404eb5241f1f0e903d9f
GobikrishnanUnited <gobi@unitdtechnologies.com>
2024-04-18 14:35:31 +0530

    first commit

--- src/views/EditData/ProductEdit.js
+++ src/views/EditData/ProductEdit.js
@@ -14,7 +14,7 @@ import ComponentCard from '../../components/ComponentCard';
 import message from '../../components/Message';
 import api from '../../constants/api';
 import creationdatetime from '../../constants/creationdatetime';
-import ProductEditButtons from '../../components/Product/ProductEditButtons';
+//import ProductEditButtons from '../../components/Product/ProductEditButtons';
 import ViewFileComponentV2 from '../../components/ProjectModal/ViewFileComponentV2';
 import AttachmentModalV2 from '../../components/Tender/AttachmentModalV2';
 import AppContext from '../../context/AppContext';
